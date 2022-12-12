4edaf48c6b14d36684146b94409e8bd5242dd3d9
dweinholz <david-weinholz@web.de>
2022-12-12 09:19:27 +0100

    fixed update script

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 (
-     flock -n 9 || exit 1
-     rm -rf /srv_root/docs/*
-     git clone $REPOSITORY /srv_root/docs
-     cd /srv_root/docs
-     mkdocs build --clean --config-file=/srv_root/docs/config.yml
+  flock -n 9 || exit 1
+  rm -rf /srv_root/docs
+  git clone $REPOSITORY /srv_root/docs
+  cd /srv_root/docs || exit
+  mkdocs build --clean --config-file=/srv_root/docs/config.yml
 ) 9>/var/lock/update.lock
